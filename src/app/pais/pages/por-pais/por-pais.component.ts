dfd36e46c21052b141a7dbe8bc13a32166539db9
VasquezNodier <vasqueznap@gmail.com>
2022-05-21 18:18:47 -0500

    Se agrefa el tipado de la peticion y se llena la tabla con los datos obtenidos

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,17 +12,21 @@ export class PorPaisComponent  {
 
   termino: string = ''
   hayError:boolean = false;
-
+  paises : Country[] = [];
   constructor(private paisService:PaisService) { }
 
   buscar(){
     this.hayError = false; 
     console.log(this.termino);
     //Para que un observable se dispare, se debe tener un suscribe
-    this.paisService.buscarPais(this.termino).subscribe(resp => {
-      console.log(resp); 
-    }, (err) => {
-      this.hayError = true;
+    this.paisService.buscarPais(this.termino).subscribe({
+      next: (paises) => {
+        this.paises = paises, 
+        console.log(paises);
+      }, 
+      error: (error) => {
+        this.hayError = true, 
+        this.paises = []},
     });
   }
 
