be75407e70e61c34a020d10d975b86d1f0e37f8a
VasquezNodier <vasqueznap@gmail.com>
2022-05-22 18:20:40 -0500

    Se usa debounceTime para quitar el error cuando se corrige

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -7,13 +9,34 @@ import { PaisService } from '../../services/pais.service';
   styles: [
   ]
 })
-export class PaisInputComponent  {
+export class PaisInputComponent implements OnInit {
 
+  
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
+
   termino:string = '';
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe( valor => {
+      this.onDebounce.emit(valor);
+      
+    })
+  }
+
 
   buscar(){
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada(){
+    this.debouncer.next(this.termino);
   }
 
 
